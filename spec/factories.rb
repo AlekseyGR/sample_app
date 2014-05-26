b736882b351a5fb63e8f13f47ea381099202ed80
LelikGR <alek.grischenko@gmail.com>
2014-05-26 10:53:53 +0400

    Finish user sign up

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "Aleksey Grischenko"
+    email "alek.grischenko@gmail.com"
+    password "as1000000009"
+    password_confirmation "as1000000009"
+  end
+end
