22866b070b358330fca527b76907bb5e520087e1
LelikGR <alek.grischenko@gmail.com>
2014-06-06 17:51:28 +0400

    Finish user edit, update, index and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -14,7 +14,7 @@ end
 
 def sign_in(user, options={})
   if options[:no_capybara]
-    #Sign in when nit using Capybara.
+    #Sign in when not using Capybara.
     remember_token = User.new_remember_token
     cookies[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.encrypt(remember_token))
