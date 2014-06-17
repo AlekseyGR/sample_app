11533bfdd65328b4a9ac3a9500491f0f608c85c4
LelikGR <alek.grischenko@gmail.com>
2014-06-17 10:15:53 +0400

    Finish user edit, update, index and destroy actions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -67,7 +67,7 @@ describe "Authentication" do
             expect(page).to have_title('Edit user')
           end
 
-          describe "when singing again" do
+          describe "when singing inagain" do
             before do
               click_link "Sign out"
               #visit signin_path
@@ -101,6 +101,23 @@ describe "Authentication" do
         end
       end
 
+      describe "as wrong user" do
+        let(:user) { FactoryGirl.create(:user) }
+        let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+        before { sign_in user, no_capybara: true }
+
+        describe "submitting a GET request to the Users#edit action" do
+          before { get edit_user_path(wrong_user) }
+          specify { expect(response.body).not_to match(full_title('Edit user')) }
+          specify { expect(response).to redirect_to(root_url) }
+        end
+
+        describe "submitting a PATCH request to the Users#update action" do
+          before { patch user_path(wrong_user) }
+          specify { expect(response).to redirect_to(root_url) }
+        end
+      end
+
       it { should_not have_link('Profile',      href: user_path(user)) }
       it { should_not have_link('Settings',     href: edit_user_path(user)) }
     end
@@ -121,22 +138,7 @@ describe "Authentication" do
       end
     end
 
-    describe "as wrong user" do
-      let(:user) { FactoryGirl.create(:user) }
-      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
-      before { sign_in user, no_capybara: true }
 
-      describe "submitting a GET request to the Users#edit action" do
-        before { get edit_user_path(wrong_user) }
-        specify { expect(response.body).not_to match(full_title('Edit user')) }
-        specify { expect(response).to redirect_to(root_url) }
-      end
-
-      describe "submitting a PATCH request to the Users#update action" do
-        before { patch user_path(wrong_user) }
-        specify { expect(response).to redirect_to(root_url) }
-      end
-    end
 
     describe "as non-admin user" do
       let(:user) { FactoryGirl.create(:user) }
